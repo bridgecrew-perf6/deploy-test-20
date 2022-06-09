81f7c53fdebba1e8b4e518562332d79ab42cf79f
HouseDust <korokoro.book1140@gmail.com>
2022-06-09 22:45:11 +0900

    環境変数見えるかテスト

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
+console.log("node_env", process.env.NODE_ENV);
+console.log("api_key", process.env.VUE_APP_API_KEY);
 </script>
 
 <style>
